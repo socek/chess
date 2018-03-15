07ddd5f10366dac04ae89d9b32e606d0171a4f95
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2018-03-15 11:18:02 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='chess',
+    version='0.1',
+    packages=find_packages(),
+    author='Py03',
+    author_email='py03@clearcode.cc',
+    zip_safe=False,
+)
